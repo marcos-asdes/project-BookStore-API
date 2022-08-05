5723fdbcc711027b4cb79dc4dd8f932c86f4c3cb
marcos.asdes <marco.engc@poli.ufrj.br>
2022-08-05 17:22:57 -0300

    build: added prisma and @prisma/client dependencies, added check auth data middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,26 @@
 import { Request, Response, NextFunction } from 'express'
+import { Prisma } from '@prisma/client'
 
-async function checkIfDataIsAlreadyRegistered (req: Request, res: Response, next: NextFunction) {
+import { AppError } from '../events/AppError.js'
+import AppLog from '../events/AppLog.js'
 
+import * as repository from '../repositories/auth.repository.js'
+
+async function checkIfDataIsAlreadyRegistered (_req: Request, res: Response, next: NextFunction) {
+  const body: Prisma.UserCreateInput = res.locals.body
+  const { email } = body
+
+  const data = await repository.findByEmail(email)
+  if (data) {
+    throw new AppError(
+      'Email already registered',
+      409,
+      'Email already registered',
+      'Ensure to provide an email address that is not already in use'
+    )
+  }
+  AppLog('Middleware', 'Email is unique')
+  next()
 }
 
 export { checkIfDataIsAlreadyRegistered }
