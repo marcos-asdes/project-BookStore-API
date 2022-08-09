813e21a46fb4c68db8585088b947951995e6ffc4
marcos.asdes <marco.engc@poli.ufrj.br>
2022-08-08 21:31:51 -0300

    env: updated .env with jwt variables; build: createdin auth.service the generateToken and the decryptPasswordfunctions

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import jwt, { Algorithm, SignOptions } from 'jsonwebtoken'
 
 import AppLog from '../events/AppLog.js'
 
@@ -9,4 +10,26 @@ function hashPassword (password: string) {
   return encrypted
 }
 
-export { hashPassword }
+function decryptPassword (password: string, encrypted: string) {
+  const passwordIsValid = bcrypt.compareSync(password, encrypted)
+
+  AppLog('Service', 'Password decrypted')
+  return passwordIsValid
+}
+
+function generateToken (id: number) {
+  const data = {}
+  const subject = id.toString()
+  const secretKey = process.env.JWT_SECRET
+  const expiresIn = process.env.JWT_EXPIRES_IN
+
+  const algorithm = process.env.JWT_ALGORITHM as Algorithm
+  const config: SignOptions = { algorithm, expiresIn, subject }
+
+  const token = jwt.sign(data, secretKey, config)
+
+  AppLog('Service', 'Token generated')
+  return token
+}
+
+export { hashPassword, decryptPassword, generateToken }
