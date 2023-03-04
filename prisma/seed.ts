07045434e1e00268a6df932d7b648a3ab4c2fefe
marcos.asdes <marco.engc@poli.ufrj.br>
2023-03-04 16:17:22 -0300

    refactor: prisma seed has been refactored

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,192 +3,211 @@ import axios from 'axios';
 import client from '../src/config/database.js';
 
 import { TBook } from '../src/types/book.js';
-import { Author, Category } from "@prisma/client";
 
 async function bookshelf() {
+  const arraySeed = ['Sapiens', 'Yuval', 'Harari', '.NET', 'dotNet', 'CSharp', 'JavaScript', 'Java', 'Typescript', 'Programação']
 
-  await client.$transaction([
-    client.$executeRaw`TRUNCATE books RESTART IDENTITY CASCADE`,
-    client.$executeRaw`TRUNCATE authors RESTART IDENTITY CASCADE`,
-    client.$executeRaw`TRUNCATE categories RESTART IDENTITY CASCADE`
-  ])
+  await resetDatabase()
 
-  const arrayTest = ['Harry+Potter', 'Sapiens', 'Javascript', 'Typescript', 'Excel', 'John+Green', 'Colleen+Hoover', 'Java', 'CSharp', 'DotNet', 'Python', 'Holmes', '.NET']
-  const arrayGoogleId: string[] = []
-  const arrayAuthors: string[] = []
-  const arrayCategories: string[] = []
+  for (let i = 0; i < arraySeed.length; i++) {
+    const googleBooksAPI = `https://www.googleapis.com/books/v1/volumes?q=${arraySeed[i]}&key=${process.env.API_KEY}`
+    await axios.get(googleBooksAPI).then(async (res) => {
+      const books: any = res.data.items
 
-  for (let i = 0; i < arrayTest.length; i++) {
-    const googleBooksAPI = `https://www.googleapis.com/books/v1/volumes?q=${arrayTest[i]}&key=${process.env.API_KEY}`
-    axios.get(googleBooksAPI).then(async (res) => {
-      let totalItems = res.data.items.length
+      for (let book of books) {
+        const googleId: string = book.id
 
-      for (let j = 0; j < totalItems; j++) {
-        const googleId: string = res.data.items[j].id
-
-        const googleIdIsDuplicated: string | undefined = arrayGoogleId.find((e: string) => e === googleId)
-        if (googleIdIsDuplicated) {
-          continue
-        }
-        arrayGoogleId.push(googleId)
-
-        const selectedItem = res.data.items[j]
-        const volumeData = selectedItem.volumeInfo
-        const saleData = selectedItem.saleInfo
-        const searchData = selectedItem.searchInfo
+        const bookAlreadyRegistered = await checkIfBookIsAlreadyRegistered(googleId)
+        if (bookAlreadyRegistered) continue
 
         // book data
-        const auxObject: TBook = {
-          google_id: '',
-          title: '',
-          publisher: null,
-          published_date: null,
-          description: null,
-          page_count: null,
-          image_link: null,
-          language: null,
-          preview_link: null,
-          saleability: 'NOT_FOR_SALE',
-          isEbook: false,
-          amount: null,
-          currency_code: null,
-          text_snippet: null
-        }
-
-        auxObject.google_id = googleId
-        auxObject.title = volumeData.title
-
-        if (volumeData.publisher) {
-          auxObject.publisher = volumeData.publisher
-        }
-
-        if (volumeData.publishedDate) {
-          auxObject.published_date = volumeData.publishedDate
-        }
-
-        if (volumeData.description) {
-          auxObject.description = volumeData.description
-        }
-
-        if (volumeData.pageCount) {
-          auxObject.page_count = volumeData.pageCount
-        }
-
-        if (volumeData.imageLinks) {
-          auxObject.image_link = volumeData.imageLinks.thumbnail
-        }
-
-        if (volumeData.language) {
-          auxObject.language = volumeData.language
-        }
-
-        if (volumeData.previewLink) {
-          auxObject.preview_link = volumeData.previewLink
-        }
-
-        if (saleData.saleability) {
-          auxObject.saleability = saleData.saleability
-        }
-
-        if (saleData.isEbook) {
-          auxObject.isEbook = saleData.isEbook
-        }
-
-        if (auxObject.saleability === 'FOR_SALE') {
-          auxObject.amount = saleData.listPrice.amount
-          auxObject.currency_code = saleData.listPrice.currencyCode
-        }
-
-        if (searchData && searchData.textSnippet) {
-          auxObject.text_snippet = searchData.textSnippet
-        }
-
-        const book = await client.book.create({
-          data: auxObject
-        })
+        const bookObject = await createBookObject(book)
+        const bookData = await createBookRegister(bookObject)
 
         // authors data
-        if (volumeData.authors && volumeData.authors.length > 0) {
-          for (let k = 0; k < volumeData.authors.length; k++) {
-            let authorAlreadyRegistered: Author
-            const authorName: string = volumeData.authors[k]
-            const authorIsDuplicated: string | undefined = arrayAuthors.find((e: string) => e === authorName)
+        if (book.volumeInfo && book.volumeInfo.authors && book.volumeInfo.authors.length > 0) {
+          const authors: any = book.volumeInfo.authors
 
-            if (authorIsDuplicated) {
-              authorAlreadyRegistered = await client.author.findUnique({
-                where: {
-                  name: authorName
-                }
-              })
-            } else {
-              arrayAuthors.push(authorName)
-            }
+          for (let author of authors) {
+            const authorAlreadyRegistered = await checkIfAuthorIsAlreadyRegistered(author)
 
-            if (!authorAlreadyRegistered) {
-              const author = await client.author.create({
-                data: {
-                  name: authorName
-                }
-              })
-              await client.bookAuthors.create({
-                data: {
-                  book_id: book.id,
-                  author_id: author.id
-                }
-              })
+            if (authorAlreadyRegistered) {
+              await createBookAuthorLink(bookData.id, authorAlreadyRegistered.id)
             } else {
-              await client.bookAuthors.create({
-                data: {
-                  book_id: book.id,
-                  author_id: authorAlreadyRegistered.id
-                }
-              })
+              const authorData = await createAuthorRegister(author)
+              await createBookAuthorLink(bookData.id, authorData.id)
             }
           }
         }
 
         // categories data
-        if (volumeData.categories && volumeData.categories.length > 0) {
-          for (let k = 0; k < volumeData.categories.length; k++) {
-            let categoryAlreadyRegistered: Category
-            const categoryName: string = volumeData.categories[k]
-            const categoryIsDuplicated: string | undefined = arrayCategories.find((e: string) => e === categoryName)
+        if (book.volumeInfo && book.volumeInfo.categories && book.volumeInfo.categories.length > 0) {
+          const categories: any = book.volumeInfo.categories
 
-            if (categoryIsDuplicated) {
-              categoryAlreadyRegistered = await client.category.findUnique({
-                where: {
-                  name: categoryName
-                }
-              })
-            } else {
-              arrayCategories.push(categoryName)
-            }
+          for (let category of categories) {
+            const categoryAlreadyRegistered = await checkIfCategoryIsAlreadyRegistered(category)
 
-            if (!categoryAlreadyRegistered) {
-              const category = await client.category.create({
-                data: {
-                  name: categoryName
-                }
-              })
-              await client.bookCategories.create({
-                data: {
-                  book_id: book.id,
-                  category_id: category.id
-                }
-              })
+            if (categoryAlreadyRegistered) {
+              await createBookCategoryLink(bookData.id, categoryAlreadyRegistered.id)
             } else {
-              await client.bookCategories.create({
-                data: {
-                  book_id: book.id,
-                  category_id: categoryAlreadyRegistered.id
-                }
-              })
+              const categoryData = await createCategoryRegister(category)
+              await createBookCategoryLink(bookData.id, categoryData.id)
             }
           }
         }
       }
     })
   }
+}
+
+async function resetDatabase() {
+  await client.$transaction([
+    client.$executeRaw`TRUNCATE books RESTART IDENTITY CASCADE`,
+    client.$executeRaw`TRUNCATE authors RESTART IDENTITY CASCADE`,
+    client.$executeRaw`TRUNCATE categories RESTART IDENTITY CASCADE`
+  ])
+}
+
+async function checkIfBookIsAlreadyRegistered(googleId: string) {
+  return await client.book.findUnique({
+    where: {
+      google_id: googleId
+    }
+  })
+}
+
+async function createBookRegister(book: TBook) {
+  return await client.book.create({
+    data: book
+  })
+}
+
+async function checkIfAuthorIsAlreadyRegistered(author: string) {
+  return await client.author.findUnique({
+    where: {
+      name: author
+    }
+  })
+}
+
+async function createAuthorRegister(author: string) {
+  return await client.author.create({
+    data: {
+      name: author
+    }
+  })
+}
+
+async function createBookAuthorLink(bookId: number, authorId: number) {
+  return await client.bookAuthors.create({
+    data: {
+      book_id: bookId,
+      author_id: authorId
+    }
+  })
+}
+
+async function checkIfCategoryIsAlreadyRegistered(category: string) {
+  return await client.category.findUnique({
+    where: {
+      name: category
+    }
+  })
+}
+
+async function createCategoryRegister(category: string) {
+  return await client.category.create({
+    data: {
+      name: category
+    }
+  })
+}
+
+async function createBookCategoryLink(bookId: number, categoryId: number) {
+  return await client.bookCategories.create({
+    data: {
+      book_id: bookId,
+      category_id: categoryId
+    }
+  })
+}
+
+async function createBookObject(book: any) {
+  const volumeData = book.volumeInfo
+  const saleData = book.saleInfo
+  const searchData = book.searchInfo
+
+  const auxObject: TBook = {
+    google_id: '',
+    title: '',
+    publisher: null,
+    published_date: null,
+    description: null,
+    page_count: null,
+    image_link: null,
+    language: null,
+    preview_link: null,
+    saleability: 'NOT_FOR_SALE',
+    isEbook: false,
+    amount: null,
+    currency_code: null,
+    text_snippet: null
+  }
+
+  auxObject.google_id = book.id
+
+  if (volumeData) {
+    auxObject.title = volumeData.title
+
+    if (volumeData.publisher) {
+      auxObject.publisher = volumeData.publisher
+    }
+
+    if (volumeData.publishedDate) {
+      auxObject.published_date = volumeData.publishedDate
+    }
+
+    if (volumeData.description) {
+      auxObject.description = volumeData.description
+    }
+
+    if (volumeData.pageCount) {
+      auxObject.page_count = volumeData.pageCount
+    }
+
+    if (volumeData.imageLinks) {
+      auxObject.image_link = volumeData.imageLinks.thumbnail
+    }
+
+    if (volumeData.language) {
+      auxObject.language = volumeData.language
+    }
+
+    if (volumeData.previewLink) {
+      auxObject.preview_link = volumeData.previewLink
+    }
+  }
+
+  if (saleData) {
+    if (saleData.saleability) {
+      auxObject.saleability = saleData.saleability
+    }
+
+    if (saleData.isEbook) {
+      auxObject.isEbook = saleData.isEbook
+    }
+
+    if (auxObject.saleability === 'FOR_SALE') {
+      auxObject.amount = saleData.listPrice.amount
+      auxObject.currency_code = saleData.listPrice.currencyCode
+    }
+  }
+
+  if (searchData && searchData.textSnippet) {
+    auxObject.text_snippet = searchData.textSnippet
+  }
+
+  return auxObject
 }
 
 bookshelf()
@@ -200,5 +219,3 @@ bookshelf()
     await client.$disconnect()
     process.exit(1)
   })
-
-console.log('Test')
